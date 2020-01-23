c922efbc9bb72ae089aa1d327379a291b64279ac
Zichen Ma <mazichen1984@gmail.com>
2020-01-23 09:21:26 -0500

    finsihed

--- demo.js
+++ demo.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const runCallback = (callback) => {
     // 如果不加 return 测试则不会通过
     // return callback();
@@ -7,4 +9,8 @@ export const runCallback = (callback) => {
 // 接收一个类，创建一个实例
 export const createObject = classItem => {
     new classItem();
+}
+
+export const getData = () => {
+    return axios.get('/api').then(res => res.data)
 }
--- demo.test.js
+++ demo.test.js
@@ -1,6 +1,8 @@
-import { runCallback, createObject } from './demo';
+import { runCallback, createObject, getData } from './demo';
+import axios from 'axios';
+jest.mock('axios');
 
-test('test runCallback', () => {
+test.only('test runCallback', () => {
     // const func = jest.fn(); // mock 函数可以捕获函数的调用
     // 如果 runCallback 被执行了，则runCallback肯定没有问题
 
@@ -21,8 +23,25 @@ test('test runCallback', () => {
     // expect(func.mock.calls[0]).toEqual(['abc']);
 
     //测试callback被执行顺序：
-    // const func = jest.fn(() => '456');
+    // const func = jest.fn(() => '456'); same as:
+    // func.mockImplementation(() => {
+    //     return '456';
+    // })
+    // return different results:
+    // func.mockImplementationOnce(() => {
+    //     return '456';
+    // })
+    // func.mockInplementationOnce(() => {
+    //     return '789'
+    // })
     const func = jest.fn();
+    // func.mockReturnThis(); // same as:
+    // func.mockImplementation(() => {
+    //     return this;
+    // })
+
+    // expect(func.mock.results[0].value).toBeUndefined();
+
     // func.mockReturnValue('dell') // 每一次都是dell
     func.mockReturnValueOnce('Dell')
     .mockReturnValueOnce('Lee')
@@ -30,9 +49,9 @@ test('test runCallback', () => {
     runCallback(func);
     runCallback(func);
     runCallback(func);
-    expect(func.mock.calls.length).toBe(3);
+    expect(func.mock.calls.length).toBe(3); //断言
     expect(func.mock.results[0].value).toBe('dell');
-    console.log(func.mock);
+    // console.log(func.mock);
     // { calls: [ [ 'abc' ], [ 'abc' ], [ 'abc' ] ],
     //   instances: [ undefined, undefined, undefined ],
     //   invocationCallOrder: [ 1, 2, 3 ],
@@ -42,16 +61,33 @@ test('test runCallback', () => {
     //      { type: 'return', value: 'Hello' } ] }
 })
 
+// 第一次调用func的参数是abc
+// expect(func.mock.calls[0]).toEqual(['abc']);
+// 每一次调用的参数都是abc
+// expect(func).toBecalledWith(['abc']);
+
 
 // instances 指的是每一次func运行的时候，他的this指向
-test.only('测试 createObject', () => {
+// test.only('测试 createObject', () => {
     // Jest.fn()的作用：
-    const func = jest.fn(); // 	1. 捕获函数的调用和返回结果，以及this和调用顺序
-    createObject(func); //  2.它可以让我们自由的设置返回结果
-    console.log(func.mock);
+    // const func = jest.fn(); // 	1. 捕获函数的调用和返回结果，以及this和调用顺序
+    // createObject(func); //  2.它可以让我们自由的设置返回结果
+    // console.log(func.mock);
     // { calls: [ [] ],
     //     instances: [ mockConstructor {} ], // 因为被new了，所以有constructord
     //     invocationCallOrder: [ 1 ],
     //     results: [ { type: 'return', value: undefined } ] }
+// })
+
+// 3. 改变函数的内部实现 (由异步请求变成模拟同步请求)
+test('Testing getData', async () => {
+    axios.get.mockResolvedValueOnce({data: 'hello'})
+    axios.get.mockResolvedValueOnce({data: 'world'})
+    await getData().then((data) => {
+        expect(data).toBe('hello');
+    });
+    await getData().then((data) => {
+        expect(data).toBe('world');
+    });
 })
 
